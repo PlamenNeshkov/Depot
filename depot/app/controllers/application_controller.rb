b957ddf4217521846ea1b2962a1ccc925e5bca20
Plamen Neshkov <plamenneshkov@ghostmail.com>
2016-03-04 04:22:57 +0200

    Woop woop

--- depot/app/controllers/application_controller.rb
+++ depot/app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :authorize
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  private
-    def current_cart
-      Cart.find(session[:cart_id])
-    rescue
-      cart = Cart.create
-      session[:cart_id] = cart.id
-      cart
+  protected
+    def authorize
+      unless User.find_by(id: session[:user_id])
+        redirect_to login_url, notice: 'Please log in'
+      end
     end
 end
--- depot/config/routes.rb
+++ depot/config/routes.rb
@@ -1,13 +1,25 @@
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  resources :users
+  resources :orders
   resources :line_items
   resources :carts
   get 'store/index'
 
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  root to: 'store#index', as: 'store'
+  root 'store#index', as: 'store'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
